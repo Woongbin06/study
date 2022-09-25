93d5f634b258fbfbfb1e0cf9be6b7ba975e851b4
Woongbin06 <kob05314@gmail.com>
2022-09-25 22:04:19 +0900

    spring transaction update v1

---
+++ jdbc/src/main/java/com/study_spring/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,72 @@
+package com.study_spring.jdbc.service;
+
+import com.study_spring.jdbc.domain.Member;
+import com.study_spring.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+// 트랜 잭션 - 트랜잭션 매니저
+@RequiredArgsConstructor
+@Slf4j
+class MemberServiceV3_1 {
+    
+//    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+    private  final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+//        Connection con = dataSource.getConnection();
+
+        // 파라미터로 속성에 관한 걸 넣어줘야함.
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            // 비지니스 로직 connectino 넘길 필요 없음.
+            bizLogic(fromId, toId, money);
+            // 커밋, 롤백
+//            con.commit(); // 성공시 커밋
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+//            con.rollback(); // 실패시 롤백
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic( String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        // fromId의 멤버를 조회후 머니를 넣고
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        // 검증
+        validation(toMember);
+        // toId의 멤버를 조회후 머니를 뺌.
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("message", e);
+            }
+        }
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
