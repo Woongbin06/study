5234f141436bf9711d8257fb8c010a798026a60b
Woongbin06 <kob05314@gmail.com>
2022-10-08 23:38:04 +0900

    spring 서비스(조회)

--- todo_list/src/main/java/com/todolist/todo_list/service/ToDoListService.java
+++ todo_list/src/main/java/com/todolist/todo_list/service/ToDoListService.java
@@ -3,16 +3,14 @@ package com.todolist.todo_list.service;
 import com.todolist.todo_list.domain.ToDoListRepository;
 import com.todolist.todo_list.domain.dto.req.ToDoListRequest;
 import com.todolist.todo_list.domain.entity.ToDoList;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ToDoListService {
 
     private final ToDoListRepository toDoListRepository;
-
-    public ToDoListService(ToDoListRepository toDoListRepository) {
-        this.toDoListRepository = toDoListRepository;
-    }
 
     public ToDoList create(ToDoListRequest request) {
         ToDoList toDoList = ToDoList.builder()
@@ -21,5 +19,10 @@ public class ToDoListService {
                 .build();
 
         return this.toDoListRepository.save(toDoList);
+    }
+
+    public ToDoList findById(Long id) {
+        return this.toDoListRepository.findById(id)
+                .orElseThrow(() -> new Exception());
     }
 }
