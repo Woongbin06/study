8069418e5ee8a2269ca5a5d4d00517e8a52b5c1d
Woongbin06 <kob05314@gmail.com>
2023-01-02 22:55:06 +0900

    ADD : User 엔티티 생성

---
+++ jwt/src/main/java/com/security/jwt/domain/user/User.java
@@ -0,0 +1,39 @@
+package com.security.jwt.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "User_tbl")
+public class User {
+
+    @Id @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 30, unique = true)
+    private String email;
+
+    @Column(length = 20)
+    private String nickname;
+
+    private int age;
+
+    @Column(length = 20)
+    private String password;
+
+    @Builder
+    public User(String email, String nickname, int age, String password) {
+        this.email = email;
+        this.nickname = nickname;
+        this.age = age;
+        this.password = password;
+    }
+}
