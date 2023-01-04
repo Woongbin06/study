00712aaf7f29a3f04b2d9e3b82da146adf0f2707
Woongbin06 <kob05314@gmail.com>
2023-01-04 14:05:25 +0900

    ADD : JwtProvider 생성

---
+++ jwt/src/main/java/com/security/jwt/global/jwt/JwtProvider.java
@@ -0,0 +1,78 @@
+package com.security.jwt.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 토큰 생성 및 검증 클래스
+ */
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    private String secretKey = "c2lsdmVybmluZS10ZWNoLXNwcmluZy1ib290LWp3dC10dXRvcmlhbC1zZWNyZXQtc2lsdmVybmluZS10ZWNoLXNwcmluZy1ib290LWp3dC10dXRvcmlhbC1zZWNyZXQK";
+
+    // 토큰 유효시간 30분
+    private Long tokenValueTime = 30 * 60 * 1000L;
+
+    private final UserDetailsService userDetailsService;
+
+    // 객체 초기화, sercetKey를 Base64로 인코딩.
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    // JWT 토큰 생성
+    public String createToken(String userPK, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userPK); // JWT payload에 저장되는 정보단위
+        claims.put("roles", roles); // 정보는 key | value 쌍으로 저장.
+        Date now = new Date(); // 날짜
+        return Jwts.builder()
+                .setClaims(claims) // 정보 저장.
+                .setIssuedAt(now) // 토큰 발행 시간 저장.
+                .setExpiration(new Date(now.getTime() + tokenValueTime)) // 현재 시간 더하기 30분
+                .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호화 알고리즘(HS256)
+                .compact();
+    }
+
+    // JWT 토큰에서 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // request의 Header에서 token값을 가져온다. "X-AUTH-TOKEN" : "TOKEN값"
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    // 토큰의 유효성 검사 + 만료일 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
